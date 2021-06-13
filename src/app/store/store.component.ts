b610d055352cba2c4e20d25820ff7923cac8bb39
uspi <snowdogloose@gmail.com>
2021-06-13 17:46:59 +0300

    added pagination functions

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -8,11 +8,16 @@ import { ProductRepository } from "../model/product.repository"
 })
 export class StoreComponent {
   public selectedCategory: string | undefined = undefined;
+  public productsPerPage = 4;
+  public selectedPage = 1;
 
   constructor(private repository: ProductRepository) { }
 
   get products(): Product[] {
-    return this.repository.getProducts(this.selectedCategory);
+    let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+
+    return this.repository.getProducts(this.selectedCategory)
+      .slice(pageIndex, pageIndex + this.productsPerPage);
   }
 
   get categories(): (string | undefined)[]{
@@ -21,5 +26,21 @@ export class StoreComponent {
 
   changeCategory(newCategory?: string){
     this.selectedCategory = newCategory;
+  }
+
+  changePage(newPage: number){
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize: number){
+    this.productsPerPage = Number(newSize);
+    this.changePage(1);
+  }
+
+  get pageNumbers(): number[]{
+    return Array(Math.ceil(this.repository
+      .getProducts(this.selectedCategory).length / this.productsPerPage))
+        .fill(0)
+        .map((x, i) => i + 1);
   }
 }
