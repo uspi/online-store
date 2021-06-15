c111b99206e378884c46e73bbe17b67463c69ef7
uspi <snowdogloose@gmail.com>
2021-06-15 22:17:50 +0300

    added counter directive and cart summary component

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { ModelModule } from '../model/model.module';
 import { StoreComponent } from './store.component';
+import { CounterDirective } from "./counter.directive";
+import { CartSummaryComponent } from './cart-summary.component';
+
 @NgModule({
   imports: [ModelModule, BrowserModule, FormsModule],
-  declarations: [StoreComponent],
+  declarations: [StoreComponent, CounterDirective, CartSummaryComponent],
   exports: [StoreComponent],
 })
 export class StoreModule {}
