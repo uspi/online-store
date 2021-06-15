05936361a0063074bc92b340e9c17ece3cff777b
uspi <snowdogloose@gmail.com>
2021-06-15 22:22:37 +0300

    added function for adding product to cart

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core"
+import { Cart } from "../model/cart.model";
 import { Product } from "../model/product.model"
 import { ProductRepository } from "../model/product.repository"
 
@@ -11,7 +12,9 @@ export class StoreComponent {
   public productsPerPage = 4;
   public selectedPage = 1;
 
-  constructor(private repository: ProductRepository) { }
+  constructor(
+    private repository: ProductRepository,
+    private cart: Cart) { }
 
   get products(): Product[] {
     let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
@@ -26,6 +29,7 @@ export class StoreComponent {
 
   changeCategory(newCategory?: string){
     this.selectedCategory = newCategory;
+    this.selectedPage = 1;
   }
 
   changePage(newPage: number){
@@ -37,10 +41,12 @@ export class StoreComponent {
     this.changePage(1);
   }
 
-  get pageNumbers(): number[]{
-    return Array(Math.ceil(this.repository
-      .getProducts(this.selectedCategory).length / this.productsPerPage))
-        .fill(0)
-        .map((x, i) => i + 1);
+  get pageCount(): number{
+    return Math.ceil(this.repository
+      .getProducts(this.selectedCategory).length / this.productsPerPage)
+  }
+
+  addProductToCart(product: Product) {
+    this.cart.addLine(product);
   }
 }
