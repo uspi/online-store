449fa692517d5eca60c930241b3198f8fd4874f9
uspi <snowdogloose@gmail.com>
2021-06-19 13:20:52 +0300

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CartDetailComponent } from './store/cart-detail.component';
+import { CheckoutComponent } from './store/checkout.component';
+import { StoreComponent } from './store/store.component';
 import { StoreModule } from './store/store.module';
 
 @NgModule({
   imports: [
-    BrowserModule,
-    StoreModule
+    BrowserModule, StoreModule,
+    RouterModule.forRoot([
+      { path: "store", component: StoreComponent },
+      { path: "cart", component: CartDetailComponent },
+      { path: "checkout", component: CheckoutComponent },
+      { path: "**", redirectTo: "/store" },
+    ])
   ],
   declarations: [
     AppComponent
